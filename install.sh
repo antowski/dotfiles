6abfd61909a645b55f88f62ecb2dc848db2decf4
antowski <antowski@gmail.com>
2016-06-19 12:39:30 +0200

    Добавлено автодополнение комманд git в tcsh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 ln -s ~/dotfiles/.tcshrc ~/.tcshrc
+ln -s /usr/local/share/git-core/contrib/completion/git-completion.tcsh  ~/.git-completion.tcsh
+ln -s /usr/local/share/git-core/contrib/completion/git-completion.bash  ~/.git-completion.bash
