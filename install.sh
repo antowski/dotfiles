cd74335b09e28121c6316076737d91daafe247eb
antowski <antowski@gmail.com>
2016-06-25 18:27:53 +0200

    .gitconfig added

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ln -s ~/dotfiles/.tcshrc ~/.tcshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.tcsh  ~/.git-completion.tcsh
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.bash  ~/.git-completion.bash
