42cf143f525fe17e16ba74b3654d811d6517f60c
antowski <antowski@gmail.com>
2017-09-23 23:22:52 +0300

    add xinitrc

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 ln -s ~/dotfiles/.tcshrc ~/.tcshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.xinitrc ~/.xinitrc
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.tcsh  ~/.git-completion.tcsh
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.bash  ~/.git-completion.bash
+ln -s ~/dotfiles/.config/mc/mc.ext ~/.config/mc/mc.ext
