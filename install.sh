99078d067ee255e73c0c6415420b86a5419eefec
antowski <antowski@gmail.com>
2017-10-08 15:21:22 +0300

    mpd settings added

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/.xinitrc ~/.xinitrc
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.tcsh  ~/.git-completion.tcsh
 ln -s /usr/local/share/git-core/contrib/completion/git-completion.bash  ~/.git-completion.bash
 ln -s ~/dotfiles/.config/mc/mc.ext ~/.config/mc/mc.ext
+ln -s ~/dotfiles/.config/mpd/mpd.conf ~/.config/mpd/mpd.conf
